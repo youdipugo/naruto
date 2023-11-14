7da03cc5bdc56f5dbf148497e7cffc1f2f33b707
youdipugo github <kzryanarinzol.ojt@gmail.com>
2023-11-14 11:27:22 +0800

    first commit

---
+++ resources/views/naruto.blade.php
@@ -0,0 +1,40 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+        <div class="wrapper-right">
+            <div class="forms">
+                
+                <h1>Registration Of Client</h1>
+                    </div>  
+                <input type="text" class="input-box" placeholder="Last name">
+                <input type="text" class="input-box" placeholder="First name">
+                <input type="text" class="input-box" placeholder="Middle name">
+                <input type="text" class="input-box" placeholder="Email">               
+               
+
+         <div>
+            <input type="text" class="input-box" placeholder="address">
+            <input type="text" class="input-box" placeholder="cellphone n#">
+            <input type="text" class="input-box" placeholder="Telephone n#">
+            <input type="text" class="input-box" placeholder="company name">   
+            <input type="button" class="btn" value="Sign Up">
+            </div>
+
+            </form> 
+        
+        
+            </div>
+        
+        </div>
+        </div>
+        
+        </body>
+        </html>
+</body>
+</html>
